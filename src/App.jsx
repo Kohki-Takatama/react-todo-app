72c6c71e931f736ab9df757cf2770d9b1c49c9e3
Kohki-Takatama <koh.takatama@gmail.com>
2024-06-18 10:37:41 +0900

    todo追加機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [isComposing, setIsComposing] = useState(false); // （日本語）変換中かを判定する
 
-
+  function handleKeyDown(event) {
+    if (event.key === 'Enter' && !isComposing) {
+      setTodos([todo, ...todos]);
+      setTodo('');
+      console.log(todos);
+    }
+  }
   return (
     <>
-
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="input your task..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        onKeyDown={handleKeyDown}
+        onCompositionStart={() => setIsComposing(true)}
+        onCompositionEnd={() => setIsComposing(false)}
+      />
+      <p>{todo}</p>
+      {todos.map((e, i) => (
+        <div key={i}>
+          <input type="checkbox" id={i} />
+          <label htmlFor={i}>{e}</label>
+        </div>
+      ))}
     </>
   );
 }
